6648a29827e49ba7875484e0b8f7c261d0b665ad
刘佳黎 <lizi@liujialideMacBook-Pro.local>
2019-02-12 21:40:26 +0800

    hidden

--- app.js
+++ app.js
@@ -1,4 +1,26 @@
 const CONF_PATH = process.argv[2];
+const fs = require('fs');
+const path = require('path');
+
 
 //遍历文件夹
+let allConfFiles = [];
+travel(CONF_PATH);
 
+function travel(folderPath){
+    fs.readdirSync(folderPath).forEach(function (file) {
+        let pathname = path.join(folderPath, file);
+        if (fs.statSync(pathname).isDirectory()) {
+            travel(pathname);
+        } else {
+            console.log(fs.statSync(pathname));
+            allConfFiles.push(pathname);
+        }
+    });
+}
+
+var isUnixHiddenPath = function (path) {
+    return (/(^|\/)\.[^\/\.]/g).test(path);
+};
+
+console.log(allConfFiles);
