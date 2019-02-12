417b5c64a301f971db93fde3d7fd96cec115b02b
刘佳黎 <lizi@liujialideMacBook-Pro.local>
2019-02-12 21:30:17 +0800

    f c

---
+++ Resolver/base.js
@@ -0,0 +1,8 @@
+class Base {
+    constructor(props) {
+        this.name = props.name;
+        this.path = props.path;
+    }
+}
+
+module.exports = Base;
---
+++ app.js
@@ -0,0 +1,4 @@
+const CONF_PATH = process.argv[2];
+
+//遍历文件夹
+
