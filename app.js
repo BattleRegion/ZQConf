61a90d0e9d5af313843610f8dbc99292eda082de
刘佳黎 <lizi@liujialideMacBook-Pro.local>
2019-02-13 17:56:13 +0800

    123

--- app.js
+++ app.js
@@ -1,11 +1,32 @@
 const CONF_PATH = process.argv[2];
 const fs = require('fs');
 const path = require('path');
-
+const Async = require('async');
 
 //遍历文件夹
 let allConfFiles = [];
+let parseTasks = [];
+let timeV = ~~(new Date().getTime()/1000);
 travel(CONF_PATH);
+for(let i = 0; i<allConfFiles.length;i++){
+    let fPath = allConfFiles[i];
+    let name = path.basename(fPath,'.xlsx');
+    if(fs.existsSync(`./resolver/${name.toLowerCase()}.js`)){
+        let resolver = require(`./resolver/${name.toLowerCase()}`);
+        let r = new resolver({
+            name:name,
+            path:fPath
+        });
+        let f = function(cb){
+            r.parse(timeV,cb);
+        };
+        parseTasks.push(f);
+    }
+}
+
+Async.series(parseTasks, (e,r)=>{
+
+});
 
 function travel(folderPath){
     fs.readdirSync(folderPath).forEach(function (file) {
@@ -13,14 +34,13 @@ function travel(folderPath){
         if (fs.statSync(pathname).isDirectory()) {
             travel(pathname);
         } else {
-            console.log(fs.statSync(pathname));
-            allConfFiles.push(pathname);
+            if(!isUnixHiddenPath(pathname)){
+                allConfFiles.push(pathname);
+            }
         }
     });
 }
 
-var isUnixHiddenPath = function (path) {
+function isUnixHiddenPath(path) {
     return (/(^|\/)\.[^\/\.]/g).test(path);
-};
-
-console.log(allConfFiles);
+}
---
+++ resolver/enemy_base.js
@@ -0,0 +1,8 @@
+const Base = require('./base');
+class EnemyBase extends Base {
+    constructor(opt){
+        super(opt);
+    }
+}
+
+module.exports = EnemyBase;
