a78adc8392906b197174ad55195982a6413e1018
刘佳黎 <lizi@liujialideiMac.local>
2019-03-21 17:07:53 +0800

    add

---
+++ resolver/card_use_condition.js
@@ -0,0 +1,12 @@
+const Base = require('./base');
+class EnemyGacha extends Base {
+    constructor(opt){
+        super(opt);
+    }
+
+    specDeal(){
+        // console.log(this.confData);
+    }
+}
+
+module.exports = EnemyGacha;
